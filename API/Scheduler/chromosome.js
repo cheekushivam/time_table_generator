7152eda4c3fd11d8a6c3eedf41f991f33cabca97
ryanStark24 <anshul2411.rs@gmail.com>
2018-03-05 18:09:43 +0530

    scheduler completed

--- API/Scheduler/chromosome.js
+++ API/Scheduler/chromosome.js
@@ -30,6 +30,7 @@ class Chromosome {
         myGene.push(myGene1.splice(0, this.DaysDescription[k].periods));
         k++;
       }
+
       // To Find that teacher priority is statisfied or not - Soft Constraint
       myGene.forEach((day, z) => {
         day.forEach((period, periodno) => { // Genes contains objects of Period class
@@ -50,9 +51,27 @@ class Chromosome {
         section.subjects.forEach((subject, i) => {
           let findLock = day.find(gene => day.indexof(gene) == subject.periodLock);
           fitness = (findLock) ? fitness + 1 : fitness - 1;
+
+          //Max period exceed per day - Hard Constraint
+          let subjectcount = day.filter(gene => gene.subject.subjectName == subject.subjectName).length;
+          fitness = (subjectcount < utility.max_periods_per_day) ? fitness + 1 : fitness - 1;
+
+          //Max period exceed per week - Hard Constraint
+          let GenePool = [];
+          for (let i = 0; i < this.Genes.length; i++) {
+            GenePool = GenePool.concat(this.Genes[i]);
+          }
+          let maxSubjectCount = GenePool.filter(gene => gene.subject.subjectName === subject.subjectName).length;
+          if (maxSubjectCount == utility.max_periods_per_week) {
+            fitness++;
+          } else if (maxSubjectCount > utility.max_periods_per_week) {
+            fitness--;
+          }
+
         });
       });
     });
+
     // Teachers clash at same period - Hard Constraint
     for (let j = 0; j < this.totalPeriods; j++) {
       let geneGroup = [];
--- API/utility.js
+++ API/utility.js
@@ -16,6 +16,8 @@
    populationSize: 1000,
    maxGeneration: 100,
    offSprings: 2,
-   suffler: 2
+   suffler: 2,
+   max_periods_per_week: 5,
+   max_periods_per_day: 2
 
  };
